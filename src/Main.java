0dc77d404846421b3ea87a15fc8ba455e0468c9d
Vladimir-Iluk <nezalejniy@ukr.net>
2025-03-12 18:09:51 +0200

    add new method "Builder"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import Builder.Director;
 import fabric_method.*;
 import  abstract_factory.*;
+import Builder.*;
 
 import java.util.Scanner;
 
@@ -11,6 +13,7 @@ public class Main {
                 System.out.println("Select patterns");
                 System.out.println("1. Factory");
                 System.out.println("2. Abstract Factory");
+                System.out.println("3. Builder");
                 System.out.println("0. Exit");
                 String command = scanner.nextLine();
                 switch (command) {
@@ -44,6 +47,27 @@ public class Main {
 
                         IButton button = factory.createButton();
                         button.paint();
+                        break;
+                    case "Builder":
+                        System.out.println("Choose car type (Sports/City):");
+                        String carType = scanner.nextLine();
+                        Director director = new Director();
+                        CarBuilder builder = new CarBuilder();
+                        CarManualBuilder manualBuilder = new CarManualBuilder();
+                        if ("Sports".equalsIgnoreCase(carType)) {
+                            director.constructSportsCar(builder);
+                            director.constructSportsCar(manualBuilder);
+                        } else if ("City".equalsIgnoreCase(carType)) {
+                            director.constructCityCar(builder);
+                            director.constructCityCar(manualBuilder);
+                        } else {
+                            System.out.println("Unknown car type");
+                            break;
+                        }
+                        Car car = builder.getResult();
+                        System.out.println("Car built:\n" + car.getCarType());
+                        Manual carManual = manualBuilder.getResult();
+                        System.out.println("\nCar manual built:\n" + carManual.print());
                         break;
                     case "Exit":
                         running = false;
